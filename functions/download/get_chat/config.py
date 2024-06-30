d67a365b512cf1bfa9065740c6b05c61aac6b699
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-06-30 12:53:53 -0300

    feat(get_chat): setup get_chat function with AWS Lambda and configuration

---
+++ functions/download/get_chat/config.py
@@ -0,0 +1,18 @@
+from infra.services import Services
+
+class GetChatConfig:
+    def __init__(self, services: Services) -> None:
+
+        function = services.aws_lambda.create_function(
+            name="GetChat",
+            path="./functions/download",
+            directory="get_chat",
+            description="Get Live chat messages and stores on DynamoDB",
+            timeout=15,
+            memory_size=512,
+            layers=[services.layers.chat_downloader_layer],
+        )
+        
+        services.sns.create_trigger("videos_topic", function)
+
+        services.dynamodb.grant_write("chats_table", function)
