5f8452202e3bba6fc549ad9e1fc8d92851ade527
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-06-30 21:37:27 -0300

    refactor(chart): modify environment variable and projection in DynamoDB query

--- functions/chart/get_chart/main.py
+++ functions/chart/get_chart/main.py
@@ -1,20 +1,17 @@
+from dataclasses import dataclass
 import json
 import os
-from dataclasses import dataclass
 
 import boto3
-
 
 @dataclass
 class Input:
     video_id: str
     interval: int = 10
 
-
 @dataclass
 class Output:
     message: str
-
 
 def lambda_handler(event, context):
 
@@ -22,12 +19,16 @@ def lambda_handler(event, context):
     interval = event["queryStringParameters"].get("interval", 10)
 
     dynamodb = boto3.resource("dynamodb")
-    TRANSCRIPTIONS_TABLE_NAME = os.environ.get("TRANSCRIPTIONS_TABLE_NAME", "Dev-Transcriptions")
+    TRANSCRIPTIONS_TABLE_NAME = os.environ.get("TRANSCRIPTIONS_TABLE_NAME", "Prod-Youtube-Live-Chat-Tracker-Live-Transcriptions")
     transcriptions_table = dynamodb.Table(TRANSCRIPTIONS_TABLE_NAME)
 
     data = transcriptions_table.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key("PK").eq(f"{video_id}#INTERVAL={interval}")
+        KeyConditionExpression=boto3.dynamodb.conditions.Key("PK").eq(f"{video_id}#INTERVAL={interval}"),
+        ProjectionExpression="SK, chat_summary, rating, reason",
     )["Items"]
+
+    for item in data:
+        item["hour"] = item.pop("SK")
 
     return {
         "statusCode": 200,
